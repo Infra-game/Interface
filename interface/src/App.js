0e1b5c7fa1e8b581faae84a8d7bb4b8f39145c17
stephane.duboze <stephane.duboze@ynov.com>
2021-10-29 17:59:09 +0200

    Init Sidebar

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-import Topbar from './component/Topbar';
+import Topbar from './component/topbar/Topbar';
+import Sidebar from './component/sidebar/Sidebar';
 
 function App() {
   return (
-    <div><Topbar/> </div>
+    <div>
+      <Topbar/>
+      <div className="container">
+        <Sidebar/>
+      </div>
+     </div>
+
   ) 
 }
 
---
+++ interface/src/component/sidebar/Sidebar.jsx
@@ -0,0 +1,35 @@
+import "./Sidebar.css";
+/*import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
+} from "@material-ui/icons";
+import { Link } from "react-router-dom";*/
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle"> Dashboard </h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+       
+    )
+}
