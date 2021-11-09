95cea7417723d5c9542cc933b3c06d3d32c40274
Black-Thor <stephane857@live.fr>
2021-11-09 01:01:22 +0100

    Correction bug Charts

--- interface/src/pages/home/Home.jsx
+++ interface/src/pages/home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home(){
     return(
         <div className="home">
             <FeaturedInfo/>
-            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+            <Chart data={userData} title="User Analytics" grid dataKey="Usage"/>
             <div className="homeWidgets">
                 <WidgetSm/>
                 <WidgetLg/>
