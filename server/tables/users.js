dd3af5348ce1323051636299532d6de5acf56539
KenTay7 <johnkta7@gmail.com>
2021-11-13 15:39:47 +0100

    delete user

--- server/tables/users.js
+++ server/tables/users.js
@@ -27,9 +27,40 @@ module.exports = (app,db) => {
                 connection.release()
 
                 if(!err) {
-                    res.json({error: false, message:`L'utilisateur ${params.username} a été ajouté !`});
+                    res.json({error: false, message:`L'utilisateur ${params.username} a été ajouté.`});
                 } else {
                     res.json({error : true, message: "L'utilisateur n'a pas pu être ajouté."});
+                    console.log(err);
+                }
+            })
+        })
+    })
+
+    app.delete('/users/:id', (req, res) => {
+        console.log("here");
+        db.getConnection((err, connection) => {
+            if(err) throw err;
+
+            connection.query('DELETE FROM users WHERE id = ?', [req.params.id], (err) => {
+                if(!err) {
+                    res.json({error : false, message : `L'utilisateur ${[req.params.id]} a été supprimé.`});
+
+                    connection.query('SELECT MAX(id) FROM users', (err, result) => {
+                        if(!err) {
+                            const maxId = result[0]["MAX(id)"]===null ? 0 : result[0]["MAX(id)"];
+
+                            connection.query('ALTER TABLE users AUTO_INCREMENT = ?', [maxId], (err) => {
+                                connection.release()
+                                if(err) {
+                                    console.log(err);
+                                }
+                            })
+                        } else {
+                            console.log(err);
+                        }
+                    })
+                } else {
+                    res.json({error : true, message : `L'utilisateur ${[req.params.id]} n'a pas pu être supprimé.`})
                     console.log(err);
                 }
             })
