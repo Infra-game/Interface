8304e1017c9ac143c47890f715712703741bb2d2
KenTay7 <johnkta7@gmail.com>
2021-12-06 18:01:16 +0100

    css fix

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,13 +8,15 @@ const Login = () => {
   const [page, setPage] = useState("login");
 
   return (
-    <div className="auth container mt-5">
-      <Navbar />
-      {page==="login" ? (
-        <LoginControl changePage={() => setPage("register")}/>
-      ) : (
-        <Signup changePage={() => setPage("login")}/>
-      )}
+    <div className="auth">
+      <div className="container mt-5">
+        <Navbar />
+        {page==="login" ? (
+          <LoginControl changePage={() => setPage("register")}/>
+        ) : (
+          <Signup changePage={() => setPage("login")}/>
+        )}
+      </div>
     </div>
   );
 }
